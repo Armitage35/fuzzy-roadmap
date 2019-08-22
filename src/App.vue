907109a8127444fc0dd86115041373e51068226c
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-08-22 11:59:08 -0400

    Iterate through the demo

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@
 	import './main.scss';
 	const demoEpics = require( './utilities/demo.js');
 
+	console.log(demoEpics.demoEpics)
+
 	export default {
 		components: {
 			Lane
@@ -21,8 +23,38 @@
 					{title: 'later', type: "later"},
 					{title: 'done', type: "done"}
 					],
-				demoEpics: demoEpics
+				demoEpics: demoEpics.demoEpics
 				}
+		},
+		methods: {
+			sortEpics(){
+				let epicsInProgress = [];
+				let epicsSoon = [];
+				let epicLater = [];
+				let epicDone = [];
+
+				for (let i = 0; i < this.demoEpics.length; i++){
+					switch (this.demoEpics[i].status){
+						case 'inProgress':
+							epicsInProgress.push(this.demoEpics[i]);
+							break;
+						case 'soon':
+							epicsSoon.push(this.demoEpics[i]);
+							break;
+						case 'later':
+							epicLater.push(this.demoEpics[i]);
+							break;
+						case 'done':
+							epicDone.push(this.demoEpics[i]);
+							break;
+						default:
+							console.log('merde')
+					}
+				};
+			}
+		},
+		created(){
+			this.sortEpics()
 		}
 	}
 </script>
