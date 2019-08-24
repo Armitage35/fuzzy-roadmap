95a8dcb24b6cfe89d40d46dcee2046af818138ec
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-08-24 12:08:47 -0400

    Apply PR updates

--- src/components/Toolbar/Toolbar.vue
+++ src/components/Toolbar/Toolbar.vue
@@ -6,8 +6,8 @@
 		</div>
 		<div class="toolbar-tools">
 			<i class="fas fa-plus-circle"></i>
-			<i class="fas fa-chart-line"></i>
 			<i class="fas fa-stream"></i>
+			<i class="fas fa-chart-line"></i>
 		</div>
 		<div class="toolbar-advancedFeatures">
 			<i class="fas fa-file-download"></i>
@@ -19,6 +19,7 @@
 </template>
 
 <script>
+	// TODO: load FA as svg to save on load time
 	export default {
 
 	}
