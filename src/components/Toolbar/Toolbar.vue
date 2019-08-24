86c1244443ffef5eb785ccc19ed1c61258f78342
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-08-24 11:24:42 -0400

    populate and style toolbar

--- src/components/Toolbar/Toolbar.vue
+++ src/components/Toolbar/Toolbar.vue
@@ -2,13 +2,18 @@
 	<div class="toolbar">
 		<div class="toolbar-roadmapName">
 			My roadmap
+			<i class="fas fa-pencil-alt"></i>
 		</div>
 		<div class="toolbar-tools">
 			<i class="fas fa-plus-circle"></i>
+			<i class="fas fa-chart-line"></i>
+			<i class="fas fa-stream"></i>
 		</div>
 		<div class="toolbar-advancedFeatures">
-			<i class="fas fa-chart-line"></i>
-			<i class="fas fa-cloud-download-alt"></i>
+			<i class="fas fa-file-download"></i>
+			<i class="fas fa-upload"></i>
+			<i class="fas fa-trash"></i>
+			<i class="fas fa-cog"></i>
 		</div>
 	</div>
 </template>
