a76c9af6087464f053c5efabd6323e249ac1651e
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-08-28 14:41:52 -0400

    Add a second column to display a profile picture

--- src/components/Modal/SettingsModal/SettingsModal.vue
+++ src/components/Modal/SettingsModal/SettingsModal.vue
@@ -1,37 +1,44 @@
 <template>
 	<div class="modal-content">
-		<div class="modal-data">
-			<div class="modal-form">
-				<label for="email">Your email</label>
-				<br />
-				<input
-					type="text"
-					name="email"
-					placeholder="ron@hogwarts.com"
-					spellcheck="true"
-					class="modal-form-title"
-					v-model="email">
+		<div class="settings-layout">
+			<div class="setting-left">
+				<div class="settings-picture"></div>
 			</div>
-			<div class="modal-form">
-				<label for="epicStatus">Preferred theme</label>
-				<br />
-				<select name="epicStatus" v-model="theme">
-					<option value="light">Light</option>
-					<option value="dark">Dark</option>
-				</select>
-			</div>
-			<div class="modal-form">
-				<label for="epicStatus">Language</label>
-				<br />
-				<select name="epicStatus" v-model="language">
-					<option value="light">English</option>
-					<option value="dark">French</option>
-				</select>
-			</div>
-			<div class="modal-form">
-				<label for="tracking">Language</label>
-				<br />
-				<input type="checkbox" name="tracking">I agree be tracked to help improve this app
+			<div class="setting-right">
+				<div class="modal-data">
+					<div class="modal-form">
+						<label for="email">Your email</label>
+						<br />
+						<input
+							type="text"
+							name="email"
+							placeholder="ron@hogwarts.com"
+							spellcheck="true"
+							class="modal-form-title"
+							v-model="email">
+					</div>
+					<div class="modal-form">
+						<label for="epicStatus">Preferred theme</label>
+						<br />
+						<select name="epicStatus" v-model="theme">
+							<option value="light">Light</option>
+							<option value="dark">Dark</option>
+						</select>
+					</div>
+					<div class="modal-form">
+						<label for="epicStatus">Language</label>
+						<br />
+						<select name="epicStatus" v-model="language">
+							<option value="light">English</option>
+							<option value="dark">French</option>
+						</select>
+					</div>
+					<div class="modal-form">
+						<label for="tracking">Language</label>
+						<br />
+						<input type="checkbox" name="tracking">I agree be tracked to help improve this app
+					</div>
+				</div>
 			</div>
 		</div>
 		<div class="modal-actions">
