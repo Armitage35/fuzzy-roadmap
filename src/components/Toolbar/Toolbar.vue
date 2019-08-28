87ea503a16eee39b582a161b6d4c30d5ea7f273a
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-08-28 09:03:15 -0400

    Add tooltip messages to toolbar (#32)
    * add tooltip messages to toolbar
    
    * Make sure to pay the linter gods

--- src/components/Toolbar/Toolbar.vue
+++ src/components/Toolbar/Toolbar.vue
@@ -2,23 +2,35 @@
 	<div class="toolbar">
 		<div class="toolbar-roadmapName">
 			<span>My roadmap</span>
-			<i class="fas fa-pencil-alt"></i>
+			<i class="fas fa-pencil-alt"
+				data-tippy="Rename this roadmap"></i>
 		</div>
-		<div class="toolbar-tools">
-			<i class="fas fa-plus-circle" @click="requestEpicCreationModal"></i>
-			<i class="fas fa-stream"></i>
-			<i class="fas fa-chart-line"></i>
+		<div class="toolbar-tools">3
+			<i class="fas fa-plus-circle"
+				@click="requestEpicCreationModal"
+				data-tippy="Create a new epic"></i>
+			<i class="fas fa-stream"
+				data-tippy="Backlog"></i>
+			<i class="fas fa-chart-line"
+				data-tippy="Reports"></i>
 		</div>
 		<div class="toolbar-advancedFeatures">
-			<i class="fas fa-file-download"></i>
-			<i class="fas fa-upload"></i>
-			<i class="fas fa-trash"></i>
-			<i class="fas fa-cog"></i>
+			<i class="fas fa-file-download"
+				data-tippy="Download roadmap"></i>
+			<i class="fas fa-upload"
+				data-tippy="Import roadmap"></i>
+			<i class="fas fa-trash"
+				data-tippy="Reset roadmap"></i>
+			<i class="fas fa-cog"
+				data-tippy="Settings"></i>
 		</div>
 	</div>
 </template>
 
 <script>
+	// eslint-disable-next-line
+	import tippy from 'tippy.js';
+
 	// @TODO: load FA as svg to save on load time
 	export default {
 		methods: {
