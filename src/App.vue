2f68ef7d7f5178f405eb23c3078e2aa62955aef1
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-08-29 09:49:06 -0400

    Update src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -52,7 +52,7 @@
 					id: Math.floor(Math.random() * 200),
 					email: 'ron@hogwarts.com',
 					profilePicture: 'https://fr.gravatar.com/userimage/26960800/576f0907a4ed387626f1c211c4b11942.png',
-					userName: 'Armitage',
+					userName: 'New User',
 					createdOn: new Date(),
 					lastLoginDate: new Date(),
 					preferences: {
