805189e5db154a6bc48bcdcb00db5ff6e66b97ac
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-08-30 11:20:06 -0400

    Delete roadmap and error messages (#54)
    * Error message when user clicks a feature not done
    
    * Generalize error message
    
    * Display danger modal
    
    * Version bump
    
    * Respect linter gods.
    
    * Be really explicit about the risks of resetting
    
    * Delete roadmap. Don't look back.

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 			:userSettings="this.userDetails"
 			:selectedEpic="this.demoEpics[this.appState.selectedEpic]"
 			@deleteEpic="deleteEpic($event)"
+			@deleteRoadmap="resetRoadmap"
 			@toggleModal="toggleModal($event)"
 			@updateEpic="updateEpic($event)"
 			@updateSettings="updateSettings($event)"
@@ -21,7 +22,7 @@
 				@epicSelected="selectEpic($event)"
 				></Lane>
 		</div>
-			<Toolbar @toggleModal="toggleModal($event)"></Toolbar>
+			<Toolbar @toggleModal="toggleModal($event)" @openResetRoadmapModal="toggleModal($event)"></Toolbar>
 	</div>
 </template>
 
@@ -46,10 +47,10 @@
 		data: function () {
 			return {
 				lanes: [
-						{title: 'in progress', type: "inProgress"},
-						{title: 'soon', type: "soon"},
-						{title: 'later', type: "later"},
-						{title: 'done', type: "done"}
+						{title: 'in progress', type: 'inProgress'},
+						{title: 'soon', type: 'soon'},
+						{title: 'later', type: 'later'},
+						{title: 'done', type: 'done'}
 					],
 				demoEpics: demoEpics.demoEpics,
 				userDetails: {
@@ -115,20 +116,20 @@
 				iziToast.success({
 					title: 'Settings updated',
 					message: 'Your profile has a newfound gleam',
-					position: "topRight"
+					position: 'topRight'
 				});
 			},
 			selectEpic(event) {
 				this.appState.selectedEpic = event;
 				this.appState.modal.showModal = true;
-				this.appState.modal.modalType = "epicDetails";
+				this.appState.modal.modalType = 'epicDetails';
 			},
 			deleteEpic(event) {
 				this.demoEpics.splice(event, 1);
 				iziToast.success({
 					title: 'Epic deleted',
 					message: 'This one\'s a goner',
-					position: "topRight"
+					position: 'topRight'
 				});
 			},
 			updateEpic(event) {
@@ -141,7 +142,16 @@
 				iziToast.success({
 					title: 'Epic updated',
 					message: 'It feels much better  already!',
-					position: "topRight"
+					position: 'topRight'
+				});
+			},
+			resetRoadmap() {
+				this.demoEpics = [];
+
+				iziToast.success({
+					title: 'Roadmap reset',
+					message: 'Well, we did warn you',
+					position: 'topRight'
 				});
 			}
 		},
--- src/components/Toolbar/Toolbar.vue
+++ src/components/Toolbar/Toolbar.vue
@@ -7,20 +7,25 @@
 		</div>
 		<div class="toolbar-tools">
 			<i class="fas fa-plus-circle"
-				@click="requestEpicCreationModal"
-				data-tippy="Create a new epic"></i>
+				data-tippy="Create a new epic"
+				@click="requestEpicCreationModal"></i>
 			<i class="fas fa-stream"
-				data-tippy="Backlog"></i>
+				data-tippy="Backlog"
+				@click="featureNotReady"></i>
 			<i class="fas fa-chart-line"
-				data-tippy="Reports"></i>
+				data-tippy="Reports"
+				@click="featureNotReady"></i>
 		</div>
 		<div class="toolbar-advancedFeatures">
 			<i class="fas fa-file-download"
-				data-tippy="Download roadmap"></i>
+				data-tippy="Download roadmap"
+				@click="featureNotReady"></i>
 			<i class="fas fa-upload"
-				data-tippy="Import roadmap"></i>
+				data-tippy="Import roadmap"
+				@click="featureNotReady"></i>
 			<i class="fas fa-trash"
-				data-tippy="Reset roadmap"></i>
+				data-tippy="Reset roadmap"
+				@click="$emit('openResetRoadmapModal', 'resetRoadmapModal')"></i>
 			<i class="fas fa-cog"
 				data-tippy="Settings"
 				@click="$emit('toggleModal', 'settings')"></i>
@@ -31,12 +36,19 @@
 <script>
 	// eslint-disable-next-line
 	import tippy from 'tippy.js';
+	import iziToast from 'izitoast';
 
-	// @TODO: load FA as svg to save on load time
 	export default {
 		methods: {
 			requestEpicCreationModal() {
-				this.$emit('toggleModal', "epic")
+				this.$emit('toggleModal', 'epic')
+			},
+			featureNotReady() {
+				iziToast.error({
+					title: 'Yikes!',
+					message: 'Sorry, I\'ve not gotten around this one yet, but be sure it\'s coming along soon!',
+					position: 'topRight'
+				});
 			}
 		}
 	}
