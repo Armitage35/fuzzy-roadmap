a9b0739dc2fed644e7edaeaaee01f8db4eefbcbb
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-09-04 10:56:15 -0400

    Boilerplate interaction

--- src/components/Modal/ImportRoadmapModal/ImportRoadmapModal.vue
+++ src/components/Modal/ImportRoadmapModal/ImportRoadmapModal.vue
@@ -3,12 +3,12 @@
 		<div class="modal-data">
 			<label for="epicName">Paste your roadmap here</label>
 			<br/>
-			<textarea name="roadmap" placeholder="Epic name...	Epic status..." wrap="off" cols="30" rows="5" v-model="epicsImport"></textarea>
+			<textarea name="roadmap" placeholder="Epic name...	Epic status..." wrap="off" cols="30" rows="5" spellcheck="false" v-model="epicsImport"></textarea>
 			<div class="modal-import-hint">Hint: paste your content from a .CSV (comma separated) file to add them to your current roadmap. We will only import your epics names and statuses. The accepted statuses can be either 'inProgress', 'soon', 'later' or 'done', anything else will be rejected.</div>
 		</div>
 		<div class="modal-actions">
-			<button type="button" class="bttn-secondary">Close</button>
-			<button type="button" class="bttn-primary">Run verification</button>
+			<button type="button" class="bttn-secondary" @click="$emit('toggleModal')">Close</button>
+			<button type="button" class="bttn-primary" @click="runVerification">Launch import</button>
 		</div>
 	</div>
 </template>
@@ -18,6 +18,11 @@
 		data: function() {
 			return {
 				epicsImport: []
+			}
+		},
+		methods: {
+			runVerification() {
+				console.log(this.epicsImport)
 			}
 		}
 	}
