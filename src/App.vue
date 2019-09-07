a99c01b2c588533d7890bacb56fc52cde8c567a4
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-09-07 11:31:24 -0400

    Load segment when user agrees

--- src/App.vue
+++ src/App.vue
@@ -52,6 +52,7 @@
 			Lane, Toolbar, Modal
 		},
 		created: function() {
+			// initialize roadmap
 			if (localStorage.getItem('roadmap') === null) {
 				this.userEpics = demoEpics.demoEpics;
 				this.saveRoadmapInClient();
@@ -59,11 +60,26 @@
 				this.userEpics = JSON.parse(localStorage.getItem('roadmap'));
 			}
 
+			// initialize user
 			if (localStorage.getItem('user') === null) {
 				this.userDetails = defaultUser.defaultUser;
 				this.saveRoadmapInClient();
 			} else {
 				this.userDetails = JSON.parse(localStorage.getItem('user'));
+			}
+		},
+		mounted: function() {
+			// segment tracking library
+			if (this.userDetails.preferences.tracking) {
+				// eslint-disable-next-line
+				!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
+				analytics.load("1RIT6LUKH0GmiuFYdtpf1Yz5oz4mrsO5");
+				analytics.page();
+				}}();
+				analytics.identify(this.userDetails.id, {
+					name: this.userDetails.username,
+					email: this.userDetails.email
+					});
 			}
 		},
 		data: function () {
