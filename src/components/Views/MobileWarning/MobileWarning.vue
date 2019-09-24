e8cc480fd6258203ac4d0f4e44b24eb62312ccdc
Adrien Dubois Ahlqvist <adrien.dubois35@gmail.com>
2019-09-24 17:34:09 -0400

    Boilerplate this new feature

---
+++ src/components/Views/MobileWarning/MobileWarning.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="mobileWarning">
+		<img src="../../../../public/android-chrome-192x192.png">
+		<h1>Sorry, this app is not made for mobiles</h1>
+		<p>Please come back once you are on a machine with a bigger screen</p>
+	</div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
